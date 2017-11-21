57a0b504997eee2844d583bf63d237b1c6f5c420
manzurahammed <manzur.ahammed236@gmail>
2017-11-21 15:34:43 +0600

    template file change

--- app.js
+++ app.js
@@ -1,26 +1,50 @@
 var app = angular.module('weather',['ngRoute']);
 app.config(function($routeProvider){
-	$routeProvider
-	.when('/',{
+	$routeProvider.when('/',{
 		templateUrl : "template/all.html",
 		controller : "home"
+	});
+	$routeProvider.when('/kl/:id',{
+		templateUrl : "template/details.html",
+		controller : "detail"
 	});
 });
 app.controller('home',function($scope,$http){
 	var city = ['Istanbul', 'Berlin', 'London', 'Helsinki', 'Dublin', 'Vancouver'];
-	var result = [];
+	$scope.result = [];
 	angular.forEach(city, function(value, key){
 		var url = 'http://localhost/weather.php?command=search&keyword='+value;
-		var r = $http.get(url)
+		$http.get(url)
 		.then(function(response){
 			if(response.status==200){
-				return response.data[0];
+				$scope.result.push(response.data[0]);
 			}
 		}, function(response) {
 			return false;
 		});
-		console.log(r.$$state.status());
-	},result);
-	console.log(result);
-	//$scope.result = result;
+	});
+});
+app.filter('tplace', function() {
+  return function(input) {
+    return parseFloat(input).toFixed(2);
+  };
+});
+app.controller('detail',function($scope,$http,$routeParams){
+	var wid = $routeParams.id;
+	if(wid==''){
+		$scope.message = 'Value Cant Empty';
+		return false;
+	}
+	var display = {};
+	var url = 'http://localhost/weather.php?command=location&woeid='+wid;
+	$http.get(url)
+	.then(function(response){
+		if(response.status==200){
+			display.consolidated_weather = response.data.consolidated_weather;
+			display.parent = response.data.parent;
+			$scope.display = display;
+		}
+	}, function(response) {
+		return false;
+	});
 });
